82aab275ea10ef0b590bff63fcc378b7387dacd5
Randy <pointdexter3949@gmail.com>
2024-01-20 21:41:36 -0500

    extract logic into functions and generalize

--- scripts/fi-formatter-scripts/bmo-chequing.bash
+++ scripts/fi-formatter-scripts/bmo-chequing.bash
@@ -1,47 +1,31 @@
 #!/bin/bash
 
 # import common functions (relative to this script)
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && . "$script_dir/fi-formatter-common.bash"
-
-# run the command
-#  ./bmo-chequing.bash bmo-chequing.csv
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && . "$script_dir/fi-formatter-common.bash"
 
 # keep the original file, create a new file for the formatted data, fill the new file with the data
 original_filename=$1
 output_directory=$2
-from_date=$3 # not used, need to add filtering by date. not a big deal as you can export by date from the bmo website
+from_date=$3
+tag_from_filename=$4
 filename="$output_directory/${original_filename%.*}-formatted.${original_filename##*.}"
 
 # copy file
 cp $original_filename $filename
 
-#  delete first 6 lines (including header, will add it back later)
-sed "1,6d" $filename >$filename.tmp && mv $filename.tmp $filename
+delete_header_and_message_lines $filename
 
-removeInvalidCharacters $filename
+remove_noise $filename
+normalize_dates $filename
+filter_by_date $filename $from_date 3
+sort_by_date $filename 3
 
-# keep the last 3 columns, remove the rest. remove 2 if there are 5 columns, remove 3 if there are 6 columns
-awk -F, '{print $(NF-2) "," $(NF-1) "," $NF}' "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+# BMO Specific
+keep_columns $filename "3,4,5"
 
-# replace date format YYYYMMDD with YYYY-MM-DD
-sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})/\1-\2-\3/g' $filename >$filename.tmp && mv $filename.tmp $filename
-
-# $3 is the from_date
-if [ -n "$3" ]; then
-    # awk remove all lines where the date is before the from_date in column 1
-    awk -F, -v from_date="$from_date" '$1 >= from_date' $filename >$filename.tmp && mv $filename.tmp $filename
-else
-    echo "From date not supplied. All transactions returned"
-fi
-
-# replace date format YYYY-MM-DD with MM/DD/YYYY
-sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})/\2\/\3\/\1/g' $filename >$filename.tmp && mv $filename.tmp $filename
-
-# add quotes, and add tag column
-sed -E 's/([0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}),(.*),(.*)/"\1","\2","\3","from BMO Mastercard"/g' $filename >$filename.tmp && mv $filename.tmp $filename
-
-# SORT FILE
-# sort -k1 -t, $filename >$filename.tmp && mv $filename.tmp $filename
-
-# ADD HEADER
-(echo '"Date","Payee","Amount","Tags"' && cat $filename) >$filename.tmp && mv $filename.tmp $filename
+# SIMPLIFI SPECIFIC
+simplifi_rearrange_columns $filename
+simplifi_add_tag_column $filename 0 # update 0 to 1 to add tag column ('from bmo-chequing')
+simplifi_date $filename
+simplifi_add_double_quotes $filename
+simplifi_add_header $filename
--- scripts/fi-formatter-scripts/fi-formatter-common.bash
+++ scripts/fi-formatter-scripts/fi-formatter-common.bash
@@ -1,24 +1,224 @@
 #!/bin/bash
 
-function removeInvalidCharacters() {
+function remove_noise() {
     local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
 
-    # replace $# with nothing
-    sed -E 's/\$//g; s/#//g' $filename >$filename.tmp && mv $filename.tmp $filename
-    
-    # replace regex (\[.*\]\s?) with nothing
-    sed -E "s/\[.*\]\s?//g" $filename >$filename.tmp && mv $filename.tmp $filename
-    # replace B/M with nothing
-    sed -E "s/B\/M//g" $filename >$filename.tmp && mv $filename.tmp $filename
+    sed -E -e 's/\$//g' \
+        -e 's/#//g' \
+        -e 's/\[.*\]\s?//g' \
+        -e 's/B\/M//g' \
+        -e 's/ +/ /g' \
+        "$filename" >"$temp_file" && mv "$temp_file" "$filename"
 
-    # replace multiple spaces with single space
-    sed -E "s/ +/ /g" $filename >$filename.tmp && mv $filename.tmp $filename
+    # remove commas between double quotes (and single quotes)
+    awk -F'["'\'']' -v OFS='"' '{ for (i=2; i<=NF; i+=2) gsub(",", "", $i) } 1' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
 
-    # remove commas between double quotes
-    awk -F'"' -v OFS='"' '{ for (i=2; i<=NF; i+=2) gsub(",", "", $i) } 1' $filename >$filename.tmp && mv $filename.tmp $filename
-    # remove double quotes
-    sed -E 's/"//g' $filename >$filename.tmp && mv $filename.tmp $filename
+    # remove single quotes, double quotes, remove leading and trailing spaces in each column. remove spaces at end of line
+    sed -E -e 's/["'\'']//g' -e 's/\, +/,/g' -e 's/ +\,/,/g' -e 's/ +$//g' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
 
-    # remove leading and trailing spaces in each column. also remove spaces at end of line
-    sed -E 's/\, +/,/g; s/ +\,/,/g; s/ +$//g;' $filename >$filename.tmp && mv $filename.tmp $filename
+function filter_by_date() {
+    local filename=$1
+    local from_date=$2
+    local date_column=$3
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    if [[ -n "$from_date" && -n "$date_column" ]]; then
+        # awk -F, -v from_date="$from_date" '$1 >= from_date' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+        awk -F, -v from_date="$from_date" -v date_column="$date_column" '$date_column >= from_date' "$filename" >"$temp_file" &&
+            mv "$temp_file" "$filename"
+
+    else
+        echo "From date not supplied. All transactions returned"
+    fi
+}
+
+# Keep only specified columns in a CSV file.
+# Columns are specified as a comma-separated string of integers.
+# For example, to keep the first, third, and fifth columns, use "1,3,5".
+function keep_columns() {
+    local input_file=$1
+    local columns_to_keep=$2
+    local temp_file=$(mktemp "$input_file.XXXXXXXXXX")
+
+    if [[ ! "$columns_to_keep" =~ ^[1-9][0-9]*(,[1-9][0-9]*)*$ ]]; then
+        echo "Error: Columns to keep must be a comma-separated string of integers starting from 1."
+        exit 1
+    fi
+
+    # Use awk to keep specified columns
+    awk -F, -v cols="${columns_to_keep}" '{
+        n = split(cols, selected_cols, ",")
+        for (i = 1; i <= NF; i++) {
+            for (j = 1; j <= n; j++) {
+                if (i == selected_cols[j]) {
+                    printf "%s%s", $i, (j == n ? "" : ",")
+                }
+            }
+        }
+        print ""
+                
+    }' "$input_file" >"$temp_file" && mv "$temp_file" "$input_file"
+}
+
+sort_by_date() {
+    local filename=$1
+    local date_column=$2
+    local reverse_sort=$3
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    if [[ -n "$date_column" ]]; then
+        if [[ "$reverse_sort" -eq 1 ]]; then
+            sort -k"$date_column" -t, -r "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+        else
+            sort -k"$date_column" -t, "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+        fi
+    else
+        echo "Date column not supplied. File not sorted"
+    fi
+}
+
+# Find the first line that matches a date pattern and delete all lines above it.
+# pattern finds a date (any format) AND a decimal number in the same line
+delete_header_and_message_lines() {
+    local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+    # date and decimal number in the same line, any order
+    date_pattern='^.*?((?:[0-9]{4}[-\/.][0-9]{2}[-\/.][0-9]{2}|[0-9]{8}|[0-9]{2}[-\/.][0-9]{2}[-\/.][0-9]{4}|[0-9]{4}[-\/.][0-9]{2}[-\/.][0-9]{2}).*?[0-9]+\.[0-9]+|[0-9]+\.[0-9]+.*?(?:[0-9]{4}[-\/.][0-9]{2}[-\/.][0-9]{2}|[0-9]{8}|[0-9]{2}[-\/.][0-9]{2}[-\/.][0-9]{4}|[0-9]{4}[-\/.][0-9]{2}[-\/.][0-9]{2})).*$'
+    # Use grep to find the line number of the first line containing a date
+    date_line=$(grep -En "$date_pattern" "$filename" | head -n 1 | cut -d ':' -f 1)
+    # Use sed to delete all lines above the line matching the regex
+    sed -n "${date_line},$ p" "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
+
+# Normalize all dates in a CSV file to YYYY-MM-DD format.
+#
+# The following date formats are supported:
+#   YYYY-MM-DD (no change)
+#   MM/DD/YYYY
+#   YYYYMMDD
+#   DD/MM/YYYY
+#   DD-MM-YYYY
+#   DD.MM.YYYY
+#   DDMMYYYY
+#   YYYY/MM/DD
+#   YYYY.MM.DD
+function normalize_dates() {
+    local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    awk -F, 'BEGIN{OFS=","} {
+        for(i=1; i<=NF; i++) {
+            # Check for YYYY-MM-DD format
+            if ($i ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/) {
+                # Do nothing
+            }
+            # Check for MM/DD/YYYY format
+            else if ($i ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {
+                split($i, date, "/")
+                $i = date[3] "-" date[1] "-" date[2]
+            }
+            # Check for YYYYMMDD format
+            else if ($i ~ /^[0-9]{8}$/) {
+                $i = substr($i, 1, 4) "-" substr($i, 5, 2) "-" substr($i, 7, 2)
+            }
+            # Check for DD/MM/YYYY format
+            else if ($i ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {
+                split($i, date, "/")
+                $i = date[3] "-" date[2] "-" date[1]
+            }
+            # Check for DD-MM-YYYY format
+            else if ($i ~ /^[0-9]{2}-[0-9]{2}-[0-9]{4}$/) {
+                split($i, date, "-")
+                $i = date[3] "-" date[2] "-" date[1]
+            }
+            # Check for DD.MM.YYYY format
+            else if ($i ~ /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/) {
+                split($i, date, ".")
+                $i = date[3] "-" date[2] "-" date[1]
+            }
+            # Check for DDMMYYYY format
+            else if ($i ~ /^[0-9]{8}$/) {
+                $i = substr($i, 1, 4) "-" substr($i, 5, 2) "-" substr($i, 7, 2)
+            }
+            # Check for YYYY/MM/DD format
+            else if ($i ~ /^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/) {
+                split($i, date, "/")
+                $i = date[1] "-" date[2] "-" date[3]
+            }
+            # Check for YYYY.MM.DD format
+            else if ($i ~ /^[0-9]{4}\.[0-9]{2}\.[0-9]{2}$/) {
+                split($i, date, ".")
+                $i = date[1] "-" date[2] "-" date[3]
+            }
+        }
+        print
+    }' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
+
+# For bulk import, add a tag column to the CSV file.
+# This makes reviewing transactions that are categoized in Simplifi easier.
+function simplifi_add_tag_column() {
+    local filename=$1
+    local tag_from_filename=$2
+    local simplifi_tag=""
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+
+    if [ "$tag_from_filename" -eq 1 ]; then
+        simplifi_tag="from $(basename -- "$filename" | sed 's/\.[^.]*$//' | sed 's/-formatted$//')"
+    fi
+
+    awk -F, -v tag="$simplifi_tag" '{print $0 "," tag}' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
+
+function simplifi_rearrange_columns() {
+    local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    # Use awk to detect column formats, rearrange columns
+    awk -F, 'BEGIN{OFS=","} {
+        # Assume initial order as date, number, string
+        date_col = $1
+        number_col = $2
+        string_col = $3
+
+        # Check the format of each column
+        if (date_col ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/ && number_col ~ /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/ && string_col ~ /^[^0-9]+$/) {
+            # Columns are in the expected format, rearrange
+            printf("%s,%s,%s", date_col, string_col, number_col)
+        } else {
+            # Columns are not in the expected format, keep as is
+            print $0
+        }
+        print ""
+    }' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
+
+function simplifi_date {
+    local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})/\2\/\3\/\1/g' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
+
+simplifi_add_double_quotes() {
+    local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    # add double quotes to all columns
+    awk -F, 'BEGIN{OFS=","} {
+        for(i=1; i<=NF; i++) {
+            $i = "\"" $i "\""
+        }
+        print
+    }' "$filename" >"$temp_file" && mv "$temp_file" "$filename"
+}
+
+function simplifi_add_header {
+    local filename=$1
+    local temp_file=$(mktemp "$filename.XXXXXXXXXX")
+
+    (echo '"Date","Payee","Amount","Tags"' && cat "$filename") >"$temp_file" && mv "$temp_file" "$filename"
 }
